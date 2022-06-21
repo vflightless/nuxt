ae1bc199028300494b2c566a86f1c15e105a8840
vflightless <vanderhorstjacob@gmail.com>
2022-06-21 13:05:29 -0700

    wew

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <Navbar />
+    <div class="text-center shadow-2xl p-10">
+      <h1 class="text-5xl">Flightless</h1>
+      <h6 class="text-2xl">Flight at the speed of run...</h6>
+    </div>
+  </section>
+</template>
+
+<script>
+import Navbar from '~/components/navbar.vue';
+export default {
+    name: "IndexPage",
+    components: { Navbar }
+}
+</script>
