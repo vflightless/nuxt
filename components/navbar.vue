291c002d0f0f23f72f766d63c8cedc8f1d90d941
vflightless <vanderhorstjacob@gmail.com>
2022-06-21 19:09:30 -0700

    ye

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,19 +1,19 @@
 <template>
-    <nav class="flex items-center justify-between flex-wrap bg-purple-500 p-6 shadow-xl">
+    <nav class="flex items-center justify-between flex-wrap text-white bg-gray-800 p-6 shadow-xl">
         <div class="flex items-center flex-shrink-0 mr-6">
-            <span class="font-semibold text-xl tracking-tight">Flightless</span>
+            <nuxt-link to="/" ><span class="font-semibold text-xl tracking-tight">Flightless</span></nuxt-link>
         </div>
         <div class="block lg:hidden">
-            <button @click="fun" class="flex items-center px-3 py-2 border rounded hover:text-white hover:border-white">
-            <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+            <button @click="fun" class="flex items-center px-3 py-2 border rounded hover:text-underline hover:border-white">
+                <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
             </button>
         </div>
         
         <div id="navtoggle" class="hidden w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-            <div class="text-sm lg:flex-grow">
-                <nuxt-link class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4" to="/">Home</nuxt-link>
-                <nuxt-link class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4" to="/about">About</nuxt-link>
-                <nuxt-link class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4" to="/blog">Blog</nuxt-link>
+            <div class="lg:flex-grow">
+                <nuxt-link class="block mt-1 rounded lg:inline-block lg:mt-0 mr-4 p-2 hover:bg-gray-900" to="/">Home</nuxt-link>
+                <nuxt-link class="block mt-1 rounded lg:inline-block lg:mt-0 mr-4 p-2 hover:bg-gray-900" to="/about">About</nuxt-link>
+                <nuxt-link class="block mt-1 rounded lg:inline-block lg:mt-0 mr-4 p-2 hover:bg-gray-900" to="/blog">Blog</nuxt-link>
             </div>
         </div>
     </nav>
@@ -23,8 +23,7 @@
     export default {
         methods: {
             fun: function navToggle() {
-                var nav = document.getElementById('navtoggle');
-                nav.classList.toggle("hidden");
+                document.getElementById('navtoggle').classList.toggle("hidden");
                 console.log("shiiiiit");
             }
         }
--- pages/blog.vue
+++ pages/blog.vue
@@ -1,5 +1,28 @@
 <template>
-    <div>
-        Blog
-    </div>
+    <section>
+        <b-container class="mx-auto">
+            <h4>Latest Posts</h4>
+            <hr />
+            <div v-for="(post, index) in posts" :key="index">
+                <time v-html="(new Date(post.sys.createdAt)).toLocaleString('en-US', {dateStyle: 'medium', timeStyle: 'short'} )"></time> - <NuxtLink :to="post.fields.slug">{{ post.fields.title }}</NuxtLink>
+            </div>
+        </b-container>
+    </section>
 </template>
+
+<script>
+import client from '~/plugins/contentful';
+
+export default ({
+    asyncData( params ) {
+        return client.getEntries({
+            content_type: 'post',
+            order: '-sys.createdAt'
+        }).then(entries => {
+            return { posts: entries.items}
+        }).catch(e => console.log(e))
+    },
+    layout: "default",
+})
+
+</script>
